2ec5b29245080a378b30d91b07523727ea4c6b3f
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-10-04 08:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ matchDrivers
 }
 
 function exactMatchToList(drivers, name) {
-  const driversNames =  {
+  const driversNames = exactMatch(drivers) {
     for (const key in name) {
       return driver[key] === name[key];
     }
