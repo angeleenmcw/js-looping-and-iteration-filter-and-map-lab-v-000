04e903135e95890570cb2cb57eb233482065538e
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-10-04 09:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(driver => driver.revenue > revenue);
+	return drivers.filter(driver => driver.revenue > revenue);
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
- const matchingDrivers = driversNamesWithRevenueOver(drivers, revenue);
- return matchingDrivers.map(driver => driver.name)
+	const matchingDrivers = driversWithRevenueOver(drivers, revenue);
+	return matchingDrivers.map(driver => driver.name);
 }
 
 function exactMatch(drivers, attribute) {
-  const matchingDrivers = drivers.filter(function(driver) {
-  for (const key in attribute) {
-    return driver[key] === attribute[key];
-}
-});
-return matchDrivers;
+	const matchingDrivers = drivers.filter(function(driver){
+		for (const key in attribute){
+			return driver[key] === attribute[key];
+		}
+	});
+	return matchingDrivers;
 }
 
 function exactMatchToList(drivers, attribute) {
-  const driversNames = exactMatch(drivers, attribute).map(driver => driver.name);
-  return driversNames;
+	const driversNames = exactMatch(drivers, attribute).map(driver => driver.name);
+	return driversNames;
 }
